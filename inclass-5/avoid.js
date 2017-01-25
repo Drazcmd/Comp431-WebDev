adea7e8bd839ed7ba600890469729d35e3be1ab1
Clayton Drazner <Drazcmd@gmail.com>
2017-01-25 01:20:10 -0600

    Stuck on event handler issues
    *Sigh* Might have to just use document.getElementById everywhere….

--- inclass-5/avoid.js
+++ inclass-5/avoid.js
@@ -1,17 +1,40 @@
-function setupButton(){
+function setupButton() {
 	console.log("sup")
 	var theButton = document.getElementById("movingButton")
-	theButton.addEventListener("click", gameWon)
+
+	//Event handler lasts for just enough time so it doesn't cause errors
+	//when resetGame() tries to remove it - can't actually event fire off.
+	theButton.addEventListener("click", resetGame)
+	resetGame()
 }
-function avoidMouse(theButton){
-	console.log("hi!")
+
+//TODO - make it only do so when specifically just the shift key
+//is pressed down	
+//TODO - Can't figure out how to deal with two conflicting problems:
+//to pass in an arg, function basically has to be anonymous. But, it
+//the function is anonymous, can't remove the event listener -_-
+function avoidMouse(){
+	var theButton = document.getElementById("movingButton")
+	console.log("TODO - avoid the mouse")
 }
 function gameWon(){
 	var theButton = document.getElementById("movingButton")
 	console.log("A winner is you!")
 	theButton.removeEventListener("click", gameWon)
 	theButton.addEventListener("click", resetGame)
-	theButton.innerHTML = "HELLOOOO"
+	theButton.innerHTML = " Reset Game "
+	console.log(theButton)
+
+	//Disable the keydown-keyup behavior until game is going again
+	document.body.removeEventListener("keydown", disableAvoid = function() {
+		console.log("avoiding disabled...")
+		theButton.removeEventListener("mouseover", avoidMouse)
+	})
+	document.body.removeEventListener("keyup", enableAvoid = function() {
+		console.log("avoiding re-enabled")
+		theButton.addEventListener("mouseover", avoidMouse)
+	})
+	theButton.removeEventListener("mouseover", avoidMouse)
 	console.log(theButton)
 }
 function resetGame(){
@@ -20,5 +43,14 @@ function resetGame(){
 	theButton.removeEventListener("click", resetGame)
 	theButton.addEventListener("click", gameWon)
 	theButton.innerHTML = " Click Me "
+	document.body.addEventListener("keydown", disableAvoid = function() {
+		console.log("avoiding disabled...")
+		theButton.removeEventListener("mouseover", avoidMouse)
+	})
+	document.body.addEventListener("keyup", enableAvoid = function() {
+		console.log("avoiding re-enabled")
+		theButton.addEventListener("mouseover", avoidMouse)
+	})
+	theButton.addEventListener("mouseover", avoidMouse)
 	console.log(theButton)
 }
