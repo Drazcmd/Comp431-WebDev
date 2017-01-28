d797daa4f0bec7271400b53e5a86bfc14010e531
Clayton Drazner <Drazcmd@gmail.com>
2017-01-27 21:57:12 -0600

    Finished main.html!
    Took a long time to do functionally, but it’s working now :) On to the
    profile page!

--- hw2/main.js
+++ hw2/main.js
@@ -7,61 +7,76 @@ var imgCardIds = [
 	"imgcard5"
 ]
 
-function showNextImages(card_images_arr){
+/**
+Mutates the image elements' .hidden attribute as needed, with
+the net effect being that the currently showing image is hid
+and the image after it in the array is shown.
+*/
+function showNextImage(card_images_arr){
 	var is_relevant_image = function (image, index){
 		if (!(image.hidden)){
 			//Want it back either if this image is not hidden...
-			console.log("I'm not hidden!", index)
 			return true
 		} else {
 			//or the previous image is not hidden
 			var num_images = card_images_arr.length
 			//(Normal js modulo handles negative numbers poorly)
 			var prev_image_index = (index - 1 + num_images) % (num_images)
-			console.log("currently", index)
-			console.log("prev", prev_image_index, !(card_images_arr[prev_image_index].hidden))
 			return !(card_images_arr[prev_image_index].hidden)
 		}
 	}
 
 	/**
-	Because we just want to operate on the two images where one is hidden
-	and one should be shown next
+	We just want to flip .hidden of the two images where image_a is visible
+	and image_b needs and needs to be shown next
 	**/
 	var relevant_images = card_images_arr.filter(is_relevant_image)
-	console.log(relevant_images)
 	var flip_hidden = function (image){
 		if (image.hidden){
 			image.hidden = false
-		} else if (!(image.hidden)) {
-			image.hidden = true	
 		} else {
-			console.log("WEIRDNESS")
-		}
+			image.hidden = true	
+		} 
+		
 	}
 	relevant_images.forEach(flip_hidden)				
 }
 
+/**
+Gives each of the image cards a working button and constructs/launches
+any neccessary functions that responsible for cycling through the images.
+*/
 function createImageCards(){
 	console.log("Hello World")
-	imgCardIds.forEach(function test(card) {
+	imgCardIds.forEach(function(card) {
 		var card = document.getElementById(card)
-		console.log(card)
-		console.log(card.children)
 		let card_images_arr = Array.from(card.children)
 
-		//Close over card_images_arr for showNextImages	
+		//Safe to add button as new child so long as we use the non-mutating 
+		//Array.from of card.children in the last line to deal with images
+		var interval_button = document.createElement("BUTTON")
+		card.appendChild(interval_button)
+
+		//Close over card_images_arr for showNextImage
 		var update_this_card = function() {
-			showNextImages(card_images_arr)
+			showNextImage(card_images_arr)
 		}
 
-		// (Assignment specified the images must change
-		// at a random interval between 0 and 5 seconds)
-		setInterval(update_this_card, Math.floor(5000 * Math.random()))
-	})
-}
+		var launch_intervals = function(){
+			// (Assignment specified the images must change
+			// at a random interval between 0 and 5 seconds)
+			interval_button.innerHTML = "Pause Images"
+			var interval_control = setInterval(update_this_card, Math.floor(5000 * Math.random()))
+			var pause_interval = function() {
+				interval_button.innerHTML = "Cycle images"
+				interval_button.removeEventListener("click", pause_interval)
+				interval_button.addEventListener("click", launch_intervals)
+	 			clearInterval(interval_control)
+			}
+			interval_button.addEventListener("click", pause_interval)
 
-/* TODO - consider launching this here
-window.onload = function() {
-	createImageCards()
-}*/
+		}
+		launch_intervals()
+	})
+
+}
