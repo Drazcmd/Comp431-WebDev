965be7030b1cd94a58fab44a869f9e7925c1f405
Clayton Drazner <Drazcmd@gmail.com>
2017-02-02 22:57:57 -0600

    Making progress
    Figured out what was going on with the whole { position } weirdness.
    It’s basically shorthand that only works if we only are going to need
    to access a position in that matter once in that scope.

--- inclass-8/src/particle.spec.js
+++ inclass-8/src/particle.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import particle, { update } from './particle'
 
 describe('Particle Functionality', () => {
-
+/*
     it('should have default values', () => {
         const p = particle({})
         expect(p).to.be.ok
@@ -27,10 +27,12 @@ describe('Particle Functionality', () => {
         //    similar to the previous check
         //    check that the velocity is updated correctly
         const p = particle({ position: [1, 1], velocity: [1, 1], acceleration:[0.5, -0.5] })
-        const { position } = update(p, 2.0) // dt is different here
+        const {position, velocity, acceleration} = update(p, 2.0)
+        expect(position).to.be.ok
+        expect(acceleration).to.eql([0.5, -0.5])
         expect(velocity).to.eql([2.0, 0.0])
     })
-
+*/
     it('particles should wrap around the world', () => {
         // IMPLEMENT ME:
         
@@ -42,27 +44,30 @@ describe('Particle Functionality', () => {
         // you will want to send the canvas into the update function
         // this means you decide the size of the canvas here.
         // canvas = { width, height }
-        const canvas = [10, 10]
+        const canvas = {width:10, height:10}
 
         const p1 =  particle({ position: [1, -0.5]})
-        update(p1, canvas)
-        const { pos1 } = update(p, 1.0, canvas) 
-        expect(pos1).to.eql([1.0, 0.0])
+        const { position } = update(p1, 1.0, canvas) 
+        expect(position).to.be.ok
+        expect(position).to.eql([1.0, 0.0])
 
+        //We can only get away with doing the shorthand {position} for accessing
+        //the returned object.position once because we don't allow mutation, and creating
+        //it more than once in the same scope would cause a name conflict
         const p2 =  particle({ position: [1, 10.5]})
-        update(p2, canvas)
-        const { pos2 } = update(p, 1.0, canvas) 
-        expect(pos2).to.eql([1.0, 10.0])
+        const p2state = update(p2, 1.0, canvas) 
+        expect(p2state.position).is.ok
+        expect(p2state.position).to.eql([1.0, 10.0])
 
-        const p3 =  particle({ position: [-0.5, 1]})
-        update(p3, canvas)
-        const { pos3 } = update(p, 1.0, canvas) 
-        expect(pos3).to.eql([0.0, 1.0])
+        const p3 = particle({ position: [-0.5, 1]})
+        const p3state = update(p3, 1.0, canvas) 
+        expect(p3state.position).is.ok
+        expect(p3state.position).to.eql([0.0, 1.0])
 
         const p4 =  particle({ position: [10.5, 1]})
-        update(p4, canvas)
-        const { pos4 } = update(p, 1.0, canvas) 
-        expect(pos4).to.eql([10.0, 1.0])
+        const p4state = update(p4, 1.0, canvas) 
+        expect(p4state.position).is.ok
+        expect(p4state.position).to.eql([0.0, 1.0])
 
 
         expect()
