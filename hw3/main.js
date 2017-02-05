56709c07f171e11cb492c91a67c5d8a2b752235c
Clayton Drazner <Drazcmd@gmail.com>
2017-02-04 23:28:29 -0600

    Initial game setup
    Realized I’d been going a pretty long time without committing - so far
    I’ve gotten the canvas up, and am correctly tracking the mouse’s x
    coordinate from the perspective of the canvas (and only when inside the
    canvas - with proper events firing on exiting/leaving it).

---
+++ hw3/main.js
@@ -0,0 +1,64 @@
+'use strict'
+
+/*
+* This function will do canvas.getContext, in addition to all the
+* other game setup-y stuff, before returning an object that lets us
+* call various subfunctions we create in here
+*/
+var createGame = function(canvas) { 
+    let c = canvas.getContext("2d");
+   	var ship = document.getElementById("spaceShip")
+
+
+	//Click and your spaceship will shoot!
+	let click = function(event) {
+		console.log("I'm a firin' mah lasah!")
+	}
+
+	//Enemy attacks
+	var nyanFire = function() {
+		console.log("NYANs ARE ATTACKING?")
+	}
+
+
+	var beginGame = function(){
+		console.log("It's a new beginning!")
+	}
+
+	// Moves towards the x coordinate of cursor (y stays the same)
+	var shipFollowsCursor = function(event) {
+		console.log("Go ship!")
+		canvas.addEventListener("mousemove", function(event) {
+			console.log(event.clientX)
+		}, false)
+	}
+	var shipStaysStill = function(event) {
+		console.log("Stop ship!")
+	}
+
+
+    return {
+    	click: click,
+    	nyanFire: nyanFire,
+    	shipFollowsCursor: shipFollowsCursor,
+    	shipStaysStill: shipStaysStill,
+    	beginGame: beginGame
+    }
+}
+
+
+
+// More or less from provided in-class 6 solution 
+window.onload = function() {
+	// This is before we've called canvas.getContext,
+	// so it can't actually 'do' anything atm.
+    var canvas = document.getElementById("gameCanvas");
+
+    // "game" is an object literal that uses closures to let us call any
+    // of the functions we set up in createGame.
+    var game = createGame(canvas)
+    canvas.addEventListener("mousedown", game.click, false)
+    canvas.addEventListener("mouseenter", game.shipFollowsCursor, false)
+    canvas.addEventListener("mouseout", game.shipStaysStill, false)
+    game.beginGame()
+}
