0d7c78cc1810aa16baac8d1757a95b4ebeda7f43
Clayton Drazner <Drazcmd@gmail.com>
2017-02-28 20:52:47 -0600

    Doing some initial setup
    Spent like 10 minutes hunting down my app after deleting a closing ‘/‘
    in the html. oops.

--- hw4/src/index.js
+++ hw4/src/index.js
@@ -9,6 +9,9 @@ import Reducer from './reducers'
 import Hello from './hello'
 
 const logger = createLogger()
+
+//Creates a Redux store that holds the complete state tree of your app.
+//There should only be a single store in your app.
 const store = createStore(Reducer, applyMiddleware(logger))
 
 render(
