d7e7c8ce748d57c4a8477b44dc0f93d0fb3fbd47
Clayton Drazner <Drazcmd@gmail.com>
2017-03-02 13:42:47 -0600

    THIS IS WHY CONNECT BROKE!!!!
    The curly braces were restructuring it into only part of what we wanted
    - and it didn’t include the connect function from it! Which is why
    connect inside the imported file (AKA button to Main) broke.

--- hw4/src/components/landing/landing.js
+++ hw4/src/components/landing/landing.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { ButtonToLanding } from '../locbuttons/buttonToLanding'
-import { ButtonToMain } from '../locbuttons/buttonToMain'
+import ButtonToLanding from '../locbuttons/buttonToLanding'
+import ButtonToMain from '../locbuttons/buttonToMain'
 export const Landing = ({ }, { }) => {
 	return (
 		<span>
