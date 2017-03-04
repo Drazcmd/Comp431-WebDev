bcb94e1640dd7f6620fec84316a87f572634f9b3
Clayton Drazner <Drazcmd@gmail.com>
2017-03-04 15:30:49 -0600

    Found the bug
    Appears to be because the default articles’ id was stored with key of
    “_id”, whereas the new ones are storing as “id”

--- hw4/src/components/main/feed.js
+++ hw4/src/components/main/feed.js
@@ -5,6 +5,9 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap'
 export const Feed = ({ articles, visibleArticleIDs }, { }) => {
 	//Oftentimes we have to display only some of the articles we have
 	//Reducing (not mapping) in case an ID sneaks in that isn't valid
+	console.log("FEED CHECKING FOR ARTICLES")
+	console.log(articles)
+	console.log(visibleArticleIDs)
 	const displayedArticles = visibleArticleIDs.reduce((acc, id) => {
 		//O(n^2), but since there shouldn't be that many on the page at
 		//once (it'd  probably have to be >>> 500 to be an issue).
@@ -13,7 +16,7 @@ export const Feed = ({ articles, visibleArticleIDs }, { }) => {
 		)
 		return searchResult.length>0 ? acc.concat(searchResult) : acc
 	}, [])
-
+	console.log(displayedArticles)
 	//Arr.sort is a litle demented in js - it directly mutates
 	//This is why I call .concat - to get us a fresh copy 
 	const sortedDisplayedArticles = displayedArticles.concat().sort(
@@ -22,10 +25,9 @@ export const Feed = ({ articles, visibleArticleIDs }, { }) => {
 				(Date.parse(articleA.date) - Date.parse(articleB.date))
 			)
 	})
-
+	console.log(sortedDisplayedArticles)
 	return (
 		<ListGroup>
-		<b>'FEED ME HERE'</b>
 		{
 			sortedDisplayedArticles.map((article, index) => (
 				<ArticleCard articleJSON={ article } key={ index } />
