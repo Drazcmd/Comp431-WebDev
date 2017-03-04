df277a3e80e2e587edf20bb315cc119d9c257d1b
Clayton Drazner <Drazcmd@gmail.com>
2017-03-04 14:48:01 -0600

    Added entry area for new people to follow
    Still gotta make it add stuff. This is gonna be a bit weird
    unfortunately since we aren’t supposed to be storing it in the global
    state I think? Not sure.

--- hw4/src/components/main/sideBar/newFollowers.js
+++ hw4/src/components/main/sideBar/newFollowers.js
@@ -1,24 +1,52 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { Button, Well, ListGroupItem } from 'react-bootstrap';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 
 export const newFollowers = ({ }) => {
+	let writeView = ""
+	function _onChange(e) {
+		writeView=e.target.value;
+	}	
+	/*
+	function _postArticle(e){
+		//accessing e.target.value here wouldn't work!
+		postArticle(writeView)
+	}
+	*/
 	return (
-		<Well>
+		<ListGroupItem> <Well>
+		<form> <FormGroup controlId="writeArticleForm">
+		  <ControlLabel> Add new followee! </ControlLabel>
 
-		HELLO WORLD
+		  <FormControl
+		   type="text" placeholder={ "Write name here..." }
+		   onChange={ _onChange }
+		   />
+		  <Button bsSize="small" onClick={ ()=>{console.log("sup")}}> 
+		  {"Add new followee"}
+		  </Button>
 
-		</Well>
+		  <Button bsSize="small" type="reset" >
+		  { "Clear above text" }
+		  </Button>
+		</FormGroup> </form>
+		</Well> </ListGroupItem>
 	)
 }
 
-
-
 newFollowers.propTypes = {
-
 }
 
+/*
+	return {
+		newFollowee(inputText) => {
+			console.log(inputText);
+			return null;
+		}
+	}
+ */
 export default connect(
     (state) => ({}), 
-    (dispatch) => ({ })
+ 	(dispatch) => ({})
 )(newFollowers)
