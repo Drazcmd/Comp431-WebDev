7805c17daa8b7857ff9f04fd06524ee56d6c7e01
Clayton Drazner <Drazcmd@gmail.com>
2017-03-04 13:15:47 -0600

    Make main a little nicer looking
    Separates mini-profile at the top out from the rest

--- hw4/src/components/main/miniProfile.js
+++ hw4/src/components/main/miniProfile.js
@@ -1,13 +1,13 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Well, Grid, Row, Col } from 'react-bootstrap'
 //This component is the picture, status, and name of the 
 //currently logged in user that appears on the main page
 export const MiniProfile = ({ profileName, profileStatus, profileImgSrc}) => {
 	const profileImgWidth="200"
 	const profileImgHeight="133"
 	return (
-		<div>
+		<Well>
 		<Col>
 	 		<img height={ profileImgHeight }
 	 	 	width={ profileImgWidth } src={ profileImgSrc } />
@@ -18,7 +18,7 @@ export const MiniProfile = ({ profileName, profileStatus, profileImgSrc}) => {
 			<h2> '{ profileStatus }' </h2>
 			</span>
 		</Col>
-		</div>
+		</Well>
 	)
 }
 
