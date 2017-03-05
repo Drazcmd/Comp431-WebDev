1af7768a9577520ac193e8d53ca5c61c451a6257
Clayton Drazner <Drazcmd@gmail.com>
2017-03-04 23:20:59 -0600

    Sweet sweet console silence
    Ok to be uploaded now. Still missing approx 11 points for lacking
    validation + updation of inputs to profile and landing page data though.

--- hw4/src/index.js
+++ hw4/src/index.js
@@ -8,13 +8,11 @@ import { createStore, applyMiddleware } from 'redux'
 import Reducer from './reducers'
 import AppRoot from './approot'
 
-const logger = createLogger()
-
 
 //Creates a Redux store that holds the complete state tree of your app.
 //There should only be a single store in your app. 
-//const store = createStore(Reducer, state={location: 'MAIN_PAGE'}, applyMiddleware(logger))
-const store = createStore(Reducer, applyMiddleware(logger))
+//const logger = createLogger()
+const store = createStore(Reducer)//, applyMiddleware(logger))
 
 //'When rendering, we will wrap our root component inside a <Provider>
 //to make the store available to all components in the component "tree"'
