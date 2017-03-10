15d7e967ae46f0d7a169130352eaee4769a566c6
Clayton Drazner <Drazcmd@gmail.com>
2017-03-10 02:25:42 -0600

    Making progress
    I think I’m understanding things

--- inclass-17/stub/profile.js
+++ inclass-17/stub/profile.js
@@ -3,9 +3,31 @@ const index = (req, res) => {
      res.send({ hello: 'world' })
 }
 
+const profile = {
+     headline: 'This is my headline!',
+     email: 'foo@bar.com',
+     zipcode: 12345,
+     avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/DWLeebron.jpg/220px-DWLeebron.jpg',
+     user = "Claytonic"
+}
+
+const headlines = (req, res) => {  
+     if (!req.user) req.user = profile.user
+     res.send({ headlines: [ 
+          { headline: profile.headline } 
+     ]}) 
+}
+
+const putHeadline = (req, res) => {
+     if (!req.headline) req.headline = profile.headline
+     if (!req.user) req.user = profile.user
+     res.send({username: req.user, headline: req.headline})
+}
+
 module.exports = app => {
      app.get('/', index)
-     app.get('/headlines/:user?', index)
+     app.get('/headlines/:user?', headlines)
+     app.put('/headline', putHeadline)
      app.get('/', index)
      app.get('/email/:user?', index)
      app.get('/', index)
