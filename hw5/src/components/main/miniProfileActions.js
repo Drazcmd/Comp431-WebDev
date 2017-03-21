01c7969407aa4ca7fbe339ca2446be14ff0b9dbe
Clayton Drazner <Drazcmd@gmail.com>
2017-03-21 17:21:40 -0500

    Basically got it
    Woohoo! Gonna move all this stuff over to the profile files though. And
    have miniProfile use the same functionality for the headline update as
    the rest of the profile data.

--- hw5/src/components/main/miniProfileActions.js
+++ hw5/src/components/main/miniProfileActions.js
@@ -9,14 +9,20 @@ sections - the mini profile's actions and the real profile's actiobns, with
 updating the status headline the only action in the mini profile's set of
 actions. but all the other profile actions
 */
-export const updateHeadline = (headline) => (dispatch) => {
-	dispatch(updateField('headline', headline))
+export const multiUpdateGenerator = (newProfileData) => {
+	return ((dispatch) => {
+	    dispatch(updateField('headline', newProfileData.headline))
+	    dispatch(updateField('email', newProfileData.email))
+	    dispatch(updateField('zipcode', newProfileData.zipcode))
+	})
 }
 
 const updateField = (field, value) => (dispatch) => {
   	const payload = { field: value }
   	resource('PUT', field, payload).then((response) => {
+  		//TODO error checking
 	 	const action = updateStatus(response.field)
 	    dispatch(action)
   	})
 }
+
--- hw5/src/components/main/miniProfileActions.spec.js
+++ hw5/src/components/main/miniProfileActions.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import mockery from 'mockery'
 import fetch, { mock } from 'mock-fetch'
 
-import { updateHeadline } from './miniProfileActions'
+import { multiUpdateGenerator } from './miniProfileActions'
 
 const url = 'https://webdev-dummy.herokuapp.com'
 let Action, actions
@@ -25,45 +25,47 @@ afterEach(() => {
 
 /**
 See piazza @138
-This is the "interesting" bit.
-This is also a "mock" of dispatch.  
+'This is the "interesting" bit. This is also a "mock" of dispatch.''
 */
-const testAction = function(action) {
+const testFieldUpdate = function(action) {
     expect(action).to.eql({
-        headline, type: actions.UPDATE_PROFILE
+        headline, type: actions.UPDATE_PROFILE_DATA
     })
   done()
 }
 
 /** 
-This just handles complex actions that call complex actions (that take
-dispatch as an argument)  the real dispatch does something similar to this 
+'This just handles complex actions that call complex actions (that take
+dispatch as an argument). The real dispatch does something similar to this'
 */
 const dispatch = function(complexAction) { 
-    return complexAction(testAction)
+    return complexAction(testFieldUpdate)
 }
 
-/**
-Note that fetching user's profile info is done both in the real profile
-file and in this 'mini' profile. However, actually updating the headline is
-only done in this 'mini' profile, and not done in the real profile. As such, I
-decided to split the 'validate profile actions' section into two separate
-sections - the mini profile's actions and the real profile's actiobns, with
-updating the status headline the only action in the mini profile's set of
-actions. but all the other profile actions
-*/
-it('should update the status message/headline', (done) => {
+it('should update the profile data all at once', (done) => {
   // the result from the mocked AJAX call
     const username = 'sep1test'
     const headline = 'A new headline!'
+    const email = 'bob@bobmail.com'
+    const zipcode = '30333'
 
-    mock(`${url}/headline`, {
-        method: 'PUT',
-        headers: {'Content-Type':'application/json'},
-        json: { username, headline }
+    const fields = ['headline', 'email', 'zipcode']
+    const fieldVals = [headline, email, zipcode]
+
+    fields.forEach((field, index) => {
+        const fieldVal = fieldVals[index]
+        //mock up the responses to the 3 fetch calls we intercept
+        mock(`${url}/${fieldVal}`, {
+            method: 'PUT',
+            headers: {'Content-Type':'application/json'},
+            json: { username, fieldVal } 
+        })
     })
 
-    // call our complex action with our mock of dispatch
-    updateHeadline(headline)(dispatch)
+    // call our complex action with our mock of dispatch, which
+    //will test our stuff using testFieldUpdate
+    multiUpdateGenerator({
+        'headline': headline, 'email': email, 'zipcode': zipcode
+    })(dispatch)
     done()
 })
