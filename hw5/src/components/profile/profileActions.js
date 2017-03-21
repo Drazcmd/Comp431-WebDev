6b2176eab65d3fa757acffa8d2b2243ca7e6ee72
Clayton Drazner <Drazcmd@gmail.com>
2017-03-21 17:23:31 -0500

    Moving mini profile test stuff to profile
    See last commit for explanation

---
+++ hw5/src/components/profile/profileActions.js
@@ -0,0 +1,28 @@
+import { updateStatus } from '../../actions'
+import { resource } from '../../dummyRequest'
+/**
+Note that fetching user's profile info is done both in the real profile
+file and in this 'mini' profile. However, actually updating the headline is
+only done in this 'mini' profile, and not done in the real profile. As such, I
+decided to split the 'validate profile actions' section into two separate
+sections - the mini profile's actions and the real profile's actiobns, with
+updating the status headline the only action in the mini profile's set of
+actions. but all the other profile actions
+*/
+export const multiUpdateGenerator = (newProfileData) => {
+	return ((dispatch) => {
+	    dispatch(updateField('headline', newProfileData.headline))
+	    dispatch(updateField('email', newProfileData.email))
+	    dispatch(updateField('zipcode', newProfileData.zipcode))
+	})
+}
+
+const updateField = (field, value) => (dispatch) => {
+  	const payload = { field: value }
+  	resource('PUT', field, payload).then((response) => {
+  		//TODO error checking
+	 	const action = updateStatus(response.field)
+	    dispatch(action)
+  	})
+}
+
---
+++ hw5/src/components/profile/profileActions.spec.js
@@ -0,0 +1,71 @@
+import { expect } from 'chai'
+import mockery from 'mockery'
+import fetch, { mock } from 'mock-fetch'
+
+import { multiUpdateGenerator } from './profileActions'
+
+const url = 'https://webdev-dummy.herokuapp.com'
+let Action, actions
+beforeEach(() => {
+    if (mockery.enable) {
+        mockery.enable({warnOnUnregistered: false, useCleanCache:true})
+        mockery.registerMock('node-fetch', fetch)
+        require('node-fetch')
+    }
+    Action = require('./../../actions').default
+    actions = require('./../../actions')
+})
+
+afterEach(() => {
+    if (mockery.enable) {
+        mockery.deregisterMock('node-fetch')
+        mockery.disable()
+    }
+})
+
+/**
+See piazza @138
+'This is the "interesting" bit. This is also a "mock" of dispatch.''
+*/
+const testFieldUpdate = function(action) {
+    expect(action).to.eql({
+        headline, type: actions.UPDATE_PROFILE_DATA
+    })
+  done()
+}
+
+/** 
+'This just handles complex actions that call complex actions (that take
+dispatch as an argument). The real dispatch does something similar to this'
+*/
+const dispatch = function(complexAction) { 
+    return complexAction(testFieldUpdate)
+}
+
+it('should update the profile data all at once', (done) => {
+  // the result from the mocked AJAX call
+    const username = 'sep1test'
+    const headline = 'A new headline!'
+    const email = 'bob@bobmail.com'
+    const zipcode = '30333'
+
+    const fields = ['headline', 'email', 'zipcode']
+    const fieldVals = [headline, email, zipcode]
+
+    fields.forEach((field, index) => {
+        const fieldVal = fieldVals[index]
+        //mock up the responses to the 3 fetch calls we intercept
+        mock(`${url}/${fieldVal}`, {
+            method: 'PUT',
+            headers: {'Content-Type':'application/json'},
+            json: { username, fieldVal } 
+        })
+    })
+
+    // call our complex action with our mock of dispatch, which
+    //will test our stuff using testFieldUpdate
+    multiUpdateGenerator({
+        'headline': headline, 'email': email, 'zipcode': zipcode
+    })(dispatch)
+    done()
+})
