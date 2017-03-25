fc44041a0f92a4e51277d22a9ad6092368bb3acd
Clayton Drazner <Drazcmd@gmail.com>
2017-03-25 16:11:37 -0500

    Moved filtering articles out of component
    Now it’s testable :)

--- hw5/src/components/main/feed.js
+++ hw5/src/components/main/feed.js
@@ -2,43 +2,9 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import ArticleCard from './articleCard'
 import { ListGroup, ListGroupItem } from 'react-bootstrap'
-import { VisModes } from './../../actions'
-
+import { displayedArticles } from './feedFilters.js'
 export const Feed = ({ articles, visibilityMode, filterStr }) => {
-    console.log(articles, visibilityMode, filterStr)
-    //TODO - move to external file
-    const filterArticles = (articles, visibilityMode, filterStr) => {
-        if (visibilityMode === VisModes.FIL_AUTH) {
-            return articles.filter(article => {
-                return article.author.includes(filterStr)
-            })
-        } else if (visibilityMode === VisModes.FIL_TEXT){
-            return articles.filter(article => {
-                return article.text.includes(filterStr)
-            })
-        } else if (visibilityMode === VisModes.NO_FILTER) {
-            return articles
-        } else {
-            //This should be an error! No reason to get here
-            return articles
-        }
-    }
-    //Arr.sort is a litle demented in js - it directly mutates
-    //This is why I call .concat - to get us a fresh copy 
-    const displayedArticles = (articles, visibilityMode, filterStr) => {
-        return filterArticles(
-            articles.concat(), visibilityMode, filterStr
-        ).sort((articleA, articleB) => {
-            //Although sorting is technically buisness logic, it's
-            //somethign that NEEDS to be delegated to this component 
-            return (-1 *
-                (Date.parse(articleA.date) -
-                Date.parse(articleB.date))
-            )
-        })
-    }
     const correctlyOrderedArticles = () => {
-        console.log(displayedArticles(articles, visibilityMode, filterStr))
         return displayedArticles(articles, visibilityMode, filterStr)
     }
     return (
