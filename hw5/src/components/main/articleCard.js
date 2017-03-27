5772f49925ed9b6cda2041346002520e3ebd8da1
Clayton Drazner <Drazcmd@gmail.com>
2017-03-26 22:27:49 -0500

    Now displaying comments
    making progress!

--- hw5/src/components/main/articleCard.js
+++ hw5/src/components/main/articleCard.js
@@ -2,49 +2,49 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { Button, Well, ListGroupItem } from 'react-bootstrap';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
-
-export const ArticleCard = ({text, image,
-	author, timestamp, postComment, editArticle
+import { CommentsHolder } from './commentsHolder'
+export const ArticleCard = ({text, image, author,
+    timestamp, comments, postComment, editArticle
 }) => {
-	const articleImage = image ? ( <img src={image}/> ) : (<div />);
-	return (
-		<ListGroupItem> <Well>
-		<div> { author, timestamp } </div>
-		{ image }
-		<div> { text } </div>
+    const articleImage = image ? ( <img src={image}/> ) : (<div />);
+    return (
+        <ListGroupItem> <Well>
+        <div> Author: {author}. Written at {timestamp} </div>
+        Image: { image }
+        <Well>Post Text: { text } </Well>
+        <div> <CommentsHolder comments={comments}/> </div>
 
-		<FormGroup>
-		  <ControlLabel />
-		  <FormControl type="text"placeholder="Enter Comment Here"/>
-		</FormGroup>
+        <FormGroup>
+            <ControlLabel />
+            <FormControl type="text"placeholder="Enter Comment Here"/>
+        </FormGroup>
 
-		<Button bsSize="small" onClick = { postComment }> { "Post Comment" }  </Button>
-		<Button bsSize="small" onClick = { editArticle }> { "Edit Article" }  </Button>
-		</Well> </ListGroupItem>
-	)
-// input type="text" defaultValue={"Comment Here "} />
+        <Button bsSize="small" onClick = { postComment }> { "Post Comment" }  </Button>
+        <Button bsSize="small" onClick = { editArticle }> { "Edit Article" }  </Button>
+        </Well> </ListGroupItem>
+    )
 }
 
 ArticleCard.propTypes = {
-	text: PropTypes.string.isRequired,
-	author: PropTypes.string.isRequired
+    author: PropTypes.string.isRequired
 }
 
 export default connect(
-	(state, ownProps) => {
-		return { 
-			//image link might be null
-			text: ownProps.articleJSON.text,
-			image: ownProps.articleJSON.img,
-			author: ownProps.articleJSON.author,
-			timestamp: ownProps.articleJSON.date
-		} 
-	},
-	(dispatch) => {
-    	return {
-    		//Currently button does nothing!
-    		postComment: () => ({}),
-    		editArticle: () => ({})
-    	}
+    (state, ownProps) => {
+        return { 
+            //image link might be null
+            text: ownProps.articleJSON.text,
+            image: ownProps.articleJSON.img,
+            author: ownProps.articleJSON.author,
+            timestamp: ownProps.articleJSON.date,
+            comments: ownProps.articleJSON.comments
+        } 
+    },
+    (dispatch) => {
+        return {
+            //Currently button does nothing!
+            postComment: () => ({}),
+            editArticle: () => ({})
+        }
     }
 )(ArticleCard)
---
+++ hw5/src/components/main/commentsHolder.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { Button, Well, ListGroupItem } from 'react-bootstrap';
+
+export const CommentsHolder = ({comments}) => {
+	//comments have an author, commentId, date, and text fields
+	console.log(comments)
+	return (
+		<Well> 
+		{"Comments: "}
+		{
+            comments.map((comment) => (
+                <div key={comment.commentId}> {comment.author}: "{comment.text}" (at {comment.date}) </div>
+            ))
+		}
+		</Well> 
+	)
+}
+
+CommentsHolder.propTypes = {
+}
+
+export default connect(
+	(state, ownProps) => {
+		return { 
+			//image link might be null
+			comments: ownProps.comments
+		} 
+	},
+	(dispatch) => {
+    	return {}
+    }
+)(CommentsHolder)
