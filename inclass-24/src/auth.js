9acf2b0b2fe6b6e0d6ac316a2cfa0a4ac8ab3483
Clayton Drazner <Drazcmd@gmail.com>
2017-04-12 01:20:47 -0500

    Hmmm...
    Not sure how to run this locally unforuntately, and the one hosted on
    heroku has a 500 internal service error when attempting logins
    unfortunately :/

--- inclass-24/src/auth.js
+++ inclass-24/src/auth.js
@@ -1,6 +1,6 @@
 const md5 = require('md5')
 const bodyParser = require('body-parser')
-const REDIS_URL = redis://h:p58afdee5f98f2e9a6d89cb8f0f284a3b46ff8644bda0c55b4b9bddc6206e451b@ec2-34-206-56-30.compute-1.amazonaws.com:45719
+const REDIS_URL = "redis://h:p58afdee5f98f2e9a6d89cb8f0f284a3b46ff8644bda0c55b4b9bddc6206e451b@ec2-34-206-56-30.compute-1.amazonaws.com:45719"
 
 exports.setup = function(app){
     app.post('/login', login)
@@ -14,8 +14,7 @@ const authMap = { }
 const cookieKey = 'sid';
 
 // - move in-memory map to a redis store
-var redis = requires('redis').createClient(process.env.REDIS_URL)
-const sessionMap = { }
+const redis = require('redis').createClient(process.env.REDIS_URL)
 
 
 const login = (req, res) => {
@@ -45,20 +44,12 @@ const login = (req, res) => {
 		const sessionId = Math.random().toString()
 		res.cookie(cookieKey, sessionId, {maxAge: 3600*1000, httpOnly: true})
 
-        //user object?
-        redis.hmset(sid, null)
+        redis.hmset(sid, userObj)
         redis.hgetall(sid, function(err, userObj) {
-            console.log(sid + 'mapped to ' + null)
+            console.log(sid + 'mapped to ' + userObj)
         })
-        //TODO - remove using the map
-		sessionMap[sessionId] = username
-
-
 		const msg = {username: username, result: 'success'}
 		res.send(msg)
-
-
-
 	} else {
 		const msg = {username: username, result: 'failure'}
 		res.send(msg)
